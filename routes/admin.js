43a104fea133c575af62aa87bc41e9d77d3e5ca2
sainttobs <sainttobs@gmail.com>
2019-05-08 12:42:01 +0100

    adding login to admin dahboard

--- routes/admin.js
+++ routes/admin.js
@@ -4,18 +4,20 @@ var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('admin/login', { title: 'Honeylane Schools | Admin' });
+	var messages = req.flash('error');
+  	res.render('admin/login', { title: 'Honeylane Schools | Admin',  messages: messages, hasErrors: messages.length > 0 });
 });
 
 
 router.post('/signin', passport.authenticate('local.signin', {
-    successRedirect: '/dashboard',
-    failureRedirect: '/',
+    successRedirect: '/admin/dashboard',
+    failureRedirect: '/admin',
     failureFlash: true
 }));
 
 router.get('/dashboard', (req, res, next) => {
-	res.render('admin/dasboard', {title: 'Honeylane Schools | Admin Dashboard'});
+	var messages = req.flash('error');
+	res.render('admin/dashboard', {title: 'Honeylane Schools | Admin Dashboard', messages: messages, hasErrors: messages.length > 0});
 })
 
 module.exports = router;
