d8c19e11850fc0a57c7ef5249173e0443df76a2e
sainttobs <sainttobs@gmail.com>
2019-05-23 19:37:03 +0100

    adding result model

--- model/result.js
+++ model/result.js
@@ -1,1 +1,10 @@
-result.js
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var resultSchema = new Schema({
+    student : {type: Schema.Types.ObjectId, ref: 'Student'},
+    subject_scores: {type: {}}
+});
+
+
+module.exports = mongoose.model('Result', resultSchema);
