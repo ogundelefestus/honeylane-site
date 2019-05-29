0286d69410858d8436e317428266279ba24f205a
Adeyefa Oluwatoba <sainttobs@gmail.com>
2019-05-29 14:34:50 -0500

    adding attendance and remarks

--- model/result.js
+++ model/result.js
@@ -6,7 +6,11 @@ var resultSchema = new Schema({
     subjects : [{type: String}],
     ca_score : [{type: Number}],
     exam_score : [{type: Number}],
-    total : [{type: Number}]
+    total : [{type: Number}],
+    remark : {type: String},
+    school_opened : {type: Number},
+    student_present : {type : Number},
+    student_absent : {type: Number}
 });
 
 
